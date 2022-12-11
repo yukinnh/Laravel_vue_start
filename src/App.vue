849bce042974685ebac832e512ed00800fbca0f9
yukinnh <yukinnh19950123@gmail.com>
2022-12-11 13:05:50 +0000

    基礎編 v-if else

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <form>
-    <div for="name">名前を入力してください</div>
-    <input type="text" id="name" v-model= "myLastName" />
-  </form>
-  <div>私の名前は、{{ myLastName }} です。</div>
+  <div id="app">
+    <form>
+      <select id ="language" v-model= "language">
+        <option value="">好きなプログラミング言語を選んでください。</option>
+        <option value="php">PHP</option>
+        <option value="ruby">Ruby</option>
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+      </select>
+    </form>
+    <div v-if="language==='php'">Web系でよく使用されている</div>
+    <div v-else-if="language==='ruby'">日本発のプログラミング言語</div>
+    <div v-else-if="language==='python'">機械学習で使用されることが多い</div>
+    <div v-else-if="language==='java'">大規模開発で用いられることが多い言語</div>
+    <div v-else>プログラミング言語が選択されていません。</div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const myLastName = ref(null);
+const language = ref(null);
 </script>
