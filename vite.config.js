5fd301859189093f2ae6a5c70a7ec4db98a0c131
yukinnh <yukinnh19950123@gmail.com>
2022-12-11 07:16:21 +0000

    サーバーのポート設定変更

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+// サーバーのポート設定の変更を追加
+  server: {
+    port: 8080
+  }
+})
